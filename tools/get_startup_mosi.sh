25ef0a5b59d795b9472d5047804b1dcf0b707e04
perigoso <perigoso@riseup.net>
2021-05-25 17:01:32 +0100

    add find and replace to get_startup_mosi.sh

--- tools/get_startup_mosi.sh
+++ tools/get_startup_mosi.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-sigrok-cli -i ../la_captures/g305_sigrok_pulseview/startup.sr -P spi:miso=MISO:mosi=MOSI:clk=SCLK:cs=CS:cpol=1:cpha=1 -A spi=mosi-data >> startup-mosi
+sigrok-cli -i ../la_captures/g305_sigrok_pulseview/startup.sr -P spi:miso=MISO:mosi=MOSI:clk=SCLK:cs=CS:cpol=1:cpha=1 -A spi=mosi-data > startup-mosi
+sed -i -e 's/spi-1: //g' startup-mosi
